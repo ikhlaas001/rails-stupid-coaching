fb454c08c29d00c258bb23ae330cfcfc9e9b9242
Ikhlaas Joondan <ikhlaasjoondan@outlook.com>
2025-09-25 19:02:27 +0400

    completed exercise

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Your custom routes
+  root to: 'questions#ask'            # root path ("/") goes to ask page
+  get 'ask', to: 'questions#ask'      # route for ask page
+  get 'answer', to: 'questions#answer' # route for answer page
 end
